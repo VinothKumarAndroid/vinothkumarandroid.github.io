d0f8d34f7808486cd6fd70bcf8ad8b4e6e4db04e
Vinoth Kumar <47516005+VinothKumarAndroid@users.noreply.github.com>
2022-08-08 12:17:31 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+function sareeList(){
+    console.log("clicked")
+    $("#saree_list_section").addClass('d-none')
+    $("#index_header").addClass("d-none")
+    $("#hr_section").addClass("d-none")
+    $("#above_footer").addClass("d-none")
+    $("#card_list_sarees").removeClass("d-none")
+}
+
+$(document).ready(function(){
+    console.log("loaded")
+})
